a1823c2e732924be416c91b8df73d473b3f4adf0
Michelle <flywindy2002@gmail.com>
2015-03-30 22:33:52 +0800

    Lecture 1: init Calculator project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Syuan Leu on 3/29/15.
+//  Copyright (c) 2015 Syuan Leu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
